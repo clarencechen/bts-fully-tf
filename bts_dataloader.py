0bd9ca6aa78a75541112bd9706bf595459b50ae4
clarencechen <clarencechenct@gmail.com>
2020-09-07 13:52:19 -0700

    refactor bts dataloader implementation

--- bts_dataloader.py
+++ bts_dataloader.py
@@ -100,23 +100,21 @@ class BtsReader(object):
 		return loader
 
 	def read_from_tf_record(self, tf_record_file, filenames_file, mode='train', num_shards=1):
-		num_data_workers = tf.data.experimental.AUTOTUNE if self.params.use_tpu else self.params.num_threads
-		# Change below hardocded value later
 		with tf.io.gfile.GFile(filenames_file, 'r') as f:
 			num_examples = len(f.readlines())
 		if num_shards > 1:
 			loader = tf.data.TFRecordDataset(['{}_part_{}'.format(tf_record_file, i) for i in range(num_shards)],
 				compression_type="GZIP",
-				num_parallel_reads=num_data_workers)
+				num_parallel_reads=tf.data.experimental.AUTOTUNE)
 		else:
 			loader = tf.data.TFRecordDataset([tf_record_file],
 				compression_type="GZIP",
-				num_parallel_reads=num_data_workers)
+				num_parallel_reads=tf.data.experimental.AUTOTUNE)
 
+		loader = loader.map(lambda x: tf.io.parse_tensor(x, tf.float32), num_parallel_calls=tf.data.experimental.AUTOTUNE)
 		if mode == 'train':
 			loader = loader.repeat().shuffle(num_examples)
 
-		loader = loader.map(lambda x: tf.io.parse_tensor(x, tf.float32), num_parallel_calls=num_data_workers)
 		return loader
 
 
@@ -124,7 +122,12 @@ class BtsDataloader(object):
 	"""bts dataloader"""
 
 	def __init__(self, params, do_rotate=False, degree=5.0, do_kb_crop=False, use_tpu=False):
-		self.params = params
+		self.batch_size = params.batch_size
+		self.height = params.height
+		self.width = params.width
+		self.dataset = params.dataset
+		self.encoder = params.encoder
+		self.tpu = params.use_tpu
 
 		self.image_mean = tf.reshape(tf.constant([123.68, 116.78, 103.94], dtype=tf.float32), [1, 1, 1, 3])
 
@@ -132,7 +135,7 @@ class BtsDataloader(object):
 		self.degree = degree
 		self.do_kb_crop = do_kb_crop
 
-	def dataset_crop_test(self, image):
+	def kb_crop(self, image):
 		height, width = tf.shape(image)[0], tf.shape(image)[1]
 		top_margin = tf.cast(height - 352, dtype='int32')
 		left_margin = tf.cast((width - 1216) / 2, dtype='int32')
@@ -141,12 +144,12 @@ class BtsDataloader(object):
 		return image
 
 	def predict_preprocess(self, batch):
-		batch.set_shape([self.params.batch_size, self.params.height, self.params.width, 3])
+		batch.set_shape([self.batch_size, self.height, self.width, 3])
 
 		batch *= 255.0
-		if self.params.encoder == 'densenet161_bts':
-			batch = (batch - self.image_mean) * 0.017
-		elif 'densenet' in self.params.encoder:
+		if self.encoder == 'densenet161_bts':
+			batch = (image_batch - self.image_mean) * 0.017
+		elif 'densenet' in self.encoder:
 			batch = preprocess_input(batch, mode='torch')
 		else:
 			batch = preprocess_input(batch, mode='tf')
@@ -156,37 +159,28 @@ class BtsDataloader(object):
 	def test_preprocess(self, batch):
 		image_batch, depth_gt_batch = batch[..., 0:3], batch[..., 3:4]
 
-		image_batch.set_shape([self.params.batch_size, self.params.height, self.params.width, 3])
-		depth_gt_batch.set_shape([self.params.batch_size, self.params.height, self.params.width, 1])
+		image_batch.set_shape([self.batch_size, self.height, self.width, 3])
+		depth_gt_batch.set_shape([self.batch_size, self.height, self.width, 1])
 
 		image_batch *= 255.0
-		if self.params.encoder == 'densenet161_bts':
+		if self.encoder == 'densenet161_bts':
 			image_batch = (image_batch - self.image_mean) * 0.017
-		elif 'densenet' in self.params.encoder:
+		elif 'densenet' in self.encoder:
 			image_batch = preprocess_input(image_batch, mode='torch')
 		else:
 			image_batch = preprocess_input(image_batch, mode='tf')
 
 		return image_batch, depth_gt_batch
 
+	def dataset_rotate(self, sample):
+		random_angle = tf.random.uniform([], - self.degree * 3.141592 / 180, self.degree * 3.141592 / 180)
+		rot_image = tfa.image.rotate(sample[..., 0:3], random_angle, interpolation='BILINEAR')
+		rot_gt_depth = tfa.image.rotate(sample[..., 3:4], random_angle, interpolation='NEAREST')
+		return tf.concat([rot_image, rot_gt_depth], axis=2)
+
 	def dataset_crop_train(self, sample):
-		if self.do_kb_crop is True:
-			height, width = tf.shape(sample)[0], tf.shape(sample)[1]
-			top_margin = tf.cast(height - 352, dtype='int32')
-			left_margin = tf.cast((width - 1216) / 2, dtype='int32')
-			sample = sample[top_margin:top_margin + 352, left_margin:left_margin + 1216, :]
-
-		# Image rotation is not supported on TPU if using tfa.image.rotate()
-		if not self.params.use_tpu and self.do_rotate is True:
-			random_angle = tf.random.uniform([], - self.degree * 3.141592 / 180, self.degree * 3.141592 / 180)
-			rot_image = tfa.image.rotate(sample[..., 0:3], random_angle, interpolation='BILINEAR')
-			rot_gt_depth = tfa.image.rotate(sample[..., 3:4], random_angle, interpolation='NEAREST')
-			sample = tf.concat([rot_image, rot_gt_depth], axis=2)
-
 		# Random cropping
-		sample = tf.image.random_crop(sample, [self.params.height, self.params.width, 4])
-
-		return sample
+		return tf.image.random_crop(sample, [self.height, self.width, 4])
 
 	def train_preprocess(self, batch):
 		# Random flipping
@@ -195,23 +189,23 @@ class BtsDataloader(object):
 		image_batch, depth_gt_batch = batch[..., 0:3], batch[..., 3:4]
 
 		# Random gamma, brightness, color augmentation
-		gamma = tf.random.uniform([self.params.batch_size, 1, 1, 1], 0.9, 1.1)
-		if self.params.dataset == 'nyu':
-			brightness = tf.random.uniform([self.params.batch_size, 1, 1, 1], 0.75, 1.25)
+		gamma = tf.random.uniform([self.batch_size, 1, 1, 1], 0.9, 1.1)
+		if self.dataset == 'nyu':
+			brightness = tf.random.uniform([self.batch_size, 1, 1, 1], 0.75, 1.25)
 		else:
-			brightness = tf.random.uniform([self.params.batch_size, 1, 1, 1], 0.9, 1.1)
-		colors = tf.random.uniform([self.params.batch_size, 1, 1, 3], 0.9, 1.1)
-		do_augment = tf.math.round(tf.random.uniform([self.params.batch_size, 1, 1, 1], 0.0, 1.0))
+			brightness = tf.random.uniform([self.batch_size, 1, 1, 1], 0.9, 1.1)
+		colors = tf.random.uniform([self.batch_size, 1, 1, 3], 0.9, 1.1)
+		do_augment = tf.random.uniform([self.batch_size, 1, 1, 1], 0.0, 1.0)
 		augmented = tf.clip_by_value((brightness * colors) * (image_batch ** gamma), 0, 1)
-		image_batch = do_augment * augmented + (1.0 - do_augment) * image_batch
+		image_batch = tf.where(do_augment > 0.5, augmented, image_batch)
 
-		image_batch.set_shape([self.params.batch_size, self.params.height, self.params.width, 3])
-		depth_gt_batch.set_shape([self.params.batch_size, self.params.height, self.params.width, 1])
+		image_batch.set_shape([self.batch_size, self.height, self.width, 3])
+		depth_gt_batch.set_shape([self.batch_size, self.height, self.width, 1])
 
 		image_batch *= 255.0
-		if self.params.encoder == 'densenet161_bts':
+		if self.encoder == 'densenet161_bts':
 			image_batch = (image_batch - self.image_mean) * 0.017
-		elif 'densenet' in self.params.encoder:
+		elif 'densenet' in self.encoder:
 			image_batch = preprocess_input(image_batch, mode='torch')
 		else:
 			image_batch = preprocess_input(image_batch, mode='tf')
@@ -219,20 +213,22 @@ class BtsDataloader(object):
 		return image_batch, depth_gt_batch
 
 	def process_dataset(self, loader, mode):
-		num_data_workers = tf.data.experimental.AUTOTUNE if self.params.use_tpu else self.params.num_threads
+		if self.do_kb_crop is True:
+			loader = loader.map(self.kb_crop, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
 		if mode == 'train':
-			loader = loader.map(self.dataset_crop_train, num_parallel_calls=num_data_workers)
-			loader = loader.batch(self.params.batch_size, drop_remainder=self.params.use_tpu)
-			loader = loader.map(self.train_preprocess, num_parallel_calls=num_data_workers)
-			loader = loader.prefetch(tf.data.experimental.AUTOTUNE)
+			# Image rotation is not supported on TPU if using tfa.image.rotate()
+			if not self.tpu and self.do_rotate is True:
+				loader = loader.map(self.dataset_rotate, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+			loader = loader.map(self.dataset_crop_train, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+			loader = loader.batch(self.batch_size, drop_remainder=self.tpu)
+			loader = loader.map(self.train_preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 		else:
-			if self.do_kb_crop is True:
-				loader = loader.map(self.dataset_crop_test, num_parallel_calls=num_data_workers)
-			loader = loader.batch(self.params.batch_size, drop_remainder=self.params.use_tpu)
+			loader = loader.batch(self.batch_size, drop_remainder=self.tpu)
 			if mode == 'test':
-				loader = loader.map(self.test_preprocess, num_parallel_calls=num_data_workers)
+				loader = loader.map(self.test_preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
 			else:
-				loader = loader.map(self.predict_preprocess, num_parallel_calls=num_data_workers)
-			loader = loader.prefetch(tf.data.experimental.AUTOTUNE)
+				loader = loader.map(self.predict_preprocess, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+		
+		loader = loader.prefetch(tf.data.experimental.AUTOTUNE)
 		return loader
